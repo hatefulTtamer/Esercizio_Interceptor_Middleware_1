d8e2c1b839dd98f452de88131a4c6f8994124e84
Ttamer <mericomattia@gmail.com>
2024-06-22 16:50:52 +0200

    corretto esercizio

--- src/main/java/co/develhope/esercizio_interceptor_middleware_1/configuration/SpringMVCConfiguration.java
+++ src/main/java/co/develhope/esercizio_interceptor_middleware_1/configuration/SpringMVCConfiguration.java
@@ -3,9 +3,11 @@ package co.develhope.esercizio_interceptor_middleware_1.configuration;
 import co.develhope.esercizio_interceptor_middleware_1.interceptor.APILoggingInterceptor;
 import co.develhope.esercizio_interceptor_middleware_1.interceptor.LegacyInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class SpringMVCConfiguration implements WebMvcConfigurer {
     @Autowired
     private APILoggingInterceptor apiLoggingInterceptor;
--- src/main/java/co/develhope/esercizio_interceptor_middleware_1/interceptor/LegacyInterceptor.java
+++ src/main/java/co/develhope/esercizio_interceptor_middleware_1/interceptor/LegacyInterceptor.java
@@ -2,8 +2,10 @@ package co.develhope.esercizio_interceptor_middleware_1.interceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 @Component
 public class LegacyInterceptor implements HandlerInterceptor {
@@ -13,5 +15,12 @@ public class LegacyInterceptor implements HandlerInterceptor {
             return false;
         }
         return true;
+    }
+
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+
+    }
+
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
     }
 }
